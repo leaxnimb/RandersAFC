f1d2517a989145a604280115ebd399201c2566f3
SanneET <eaa23set@students.eaaa.dk>
2024-12-15 16:54:59 +0100

    kommentar

--- js/tilmelding.js
+++ js/tilmelding.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//tilføjer en funktion der sørger for at når der bliver clikket så gøre den til den angivet side.
+//henter html elemenet med id og tilføjer en eventlistner på click som derefter udføre den givet funktion.
 document.getElementById('tilmelding-btn').addEventListener('click', function() {
-    window.location.href = 'index.html'; 
+    window.location.href = 'signup.html'; 
 });
