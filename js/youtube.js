ba1ba6b9c86ab728d11b64570974fbb32c4ece08
IdaASU <eaa23iasu@students.eaaa.dk>
2024-12-24 14:04:11 +0100

    youtube men tekstbokse hader mig

---
+++ js/youtube.js
@@ -0,0 +1,20 @@
+"use strict";
+const videoUrl = "https://youtu.be/Lhka0pjBzAQ";
+    // Erstat dette med det ønskede video-URL
+const videoId = "dQw4w9WgXcQ"; // Erstat dette med det ønskede video-ID
+
+    // Containeren, hvor videoen skal indsættes
+    const videoContainer = document.getElementById("video-placeholder");
+
+    // Opret iframe-elementet dynamisk
+    const iframe = document.createElement("iframe");
+    iframe.width = "560";
+    iframe.height = "315";
+    iframe.src = `https://www.youtube.com/embed/${videoId}`; // Brug embed-formatet
+    iframe.title = "YouTube video player";
+    iframe.frameBorder = "0";
+    iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+    iframe.allowFullscreen = true;
+
+    // Tilføj iframe til containeren
+    videoContainer.appendChild(iframe);
